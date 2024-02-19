0602a7d907cb9c02e212f1aec1e797acbdbc4272
Pags <programwithpags@gmail.com>
2024-02-19 10:47:27 -0600

    update

--- layouts/default.layout.tsx
+++ layouts/default.layout.tsx
@@ -1,12 +1,8 @@
-import BaseWrapper from "@/components/BaseWrapper";
-import React from "react";
-import BaseConnectWallet from "@/components/BaseConnectWallet";
+import BaseWrapper from '@/components/BaseWrapper';
+import React from 'react';
+import BaseConnectWallet from '@/components/BaseConnectWallet';
 
-export default function DefaultLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <BaseWrapper>
       <BaseConnectWallet />
